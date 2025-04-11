8fab4f62f3d02d65b4b1a992f869ad0018e4f345
NahumFGz <ngutierrez2507@gmail.com>
2025-04-11 04:35:37 -0500

    feat: :sparkles: Terminando la configuración del ORM

--- b_tutorial/backend/src/config/typeorm.config.ts
+++ b_tutorial/backend/src/config/typeorm.config.ts
@@ -1,5 +1,15 @@
+import { ConfigService } from '@nestjs/config';
 import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const typeOrmConfig = (): TypeOrmModuleOptions => ({
+export const typeOrmConfig = (
+  configService: ConfigService,
+): TypeOrmModuleOptions => ({
   type: 'postgres',
+  host: configService.get('DATABASE_HOST'),
+  port: configService.get('DATABASE_PORT'),
+  username: configService.get('DATABASE_USER'),
+  password: configService.get('DATABASE_PASS'),
+  name: configService.get('DATABASE_NAME'),
+  //ssl: true, //! Solo usar si solicita SSL
+  logging: true,
 });

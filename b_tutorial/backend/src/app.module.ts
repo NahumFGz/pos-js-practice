c45025179a3fd3d8807a30c48dfafa36511b2f8e
NahumFGz <ngutierrez2507@gmail.com>
2025-04-11 03:25:59 -0500

    feat: :sparkles: Creando modulo, controlador y categoría

--- b_tutorial/backend/src/app.module.ts
+++ b_tutorial/backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
   //exports: [AppService], //! Con el exports se hace q el service esté disponible donde se importe y no solo donde el módulo

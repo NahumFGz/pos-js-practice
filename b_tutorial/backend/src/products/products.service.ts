db185969d6f7dec612b6ecfd17cbc0326b9a9c74
NahumFGz <ngutierrez2507@gmail.com>
2025-04-12 18:53:18 -0500

    feat: :sparkles: Agregando la categoría y errores

--- b_tutorial/backend/src/products/products.service.ts
+++ b_tutorial/backend/src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { CreateProductDto } from './dto/create-product.dto'
 import { UpdateProductDto } from './dto/update-product.dto'
 import { InjectRepository } from '@nestjs/typeorm'
@@ -16,8 +16,18 @@ export class ProductsService {
     private readonly categoryRepository: Repository<Category>,
   ) {}
 
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product'
+  async create(createProductDto: CreateProductDto) {
+    const category = await this.categoryRepository.findOneBy({
+      id: createProductDto.categoryId,
+    })
+
+    if (!category) {
+      const errors: string[] = []
+      errors.push('La categoría no existe')
+      throw new NotFoundException(errors)
+    }
+
+    console.log(category)
   }
 
   findAll() {

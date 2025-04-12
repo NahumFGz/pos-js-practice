b82463fdeb30192a03a845afdd5a421880e94071
NahumFGz <ngutierrez2507@gmail.com>
2025-04-12 04:34:01 -0500

    fix: :bug: Corrección de la configuración del database de typeORM

--- b_tutorial/backend/src/config/typeorm.config.ts
+++ b_tutorial/backend/src/config/typeorm.config.ts
@@ -1,5 +1,6 @@
 import { ConfigService } from '@nestjs/config';
 import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { join } from 'path';
 
 export const typeOrmConfig = (
   configService: ConfigService,
@@ -9,7 +10,9 @@ export const typeOrmConfig = (
   port: configService.get('DATABASE_PORT'),
   username: configService.get('DATABASE_USER'),
   password: configService.get('DATABASE_PASS'),
-  name: configService.get('DATABASE_NAME'),
+  database: configService.get('DATABASE_NAME'),
   //ssl: true, //! Solo usar si solicita SSL
   logging: true,
+  entities: [join(__dirname + '../../**/*.entity.{js,ts}')], //! Buscar los entities
+  synchronize: true, //! Genera las tablas [no usar en produccion]
 });

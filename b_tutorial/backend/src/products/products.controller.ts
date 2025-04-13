416d22177337d08115c4c0d36811b7b6b1f269be
NahumFGz <ngutierrez2507@gmail.com>
2025-04-13 15:58:57 -0500

    feat: :sparkles: Agregando el put

--- b_tutorial/backend/src/products/products.controller.ts
+++ b_tutorial/backend/src/products/products.controller.ts
@@ -3,15 +3,16 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
   Query,
+  Put,
 } from '@nestjs/common'
 import { ProductsService } from './products.service'
 import { CreateProductDto } from './dto/create-product.dto'
 import { UpdateProductDto } from './dto/update-product.dto'
 import { GetProductsQueryDto } from './dto/get-product.dto'
+import { IdValidationPipe } from 'src/common/pipes/id-validation/id-validation.pipe'
 
 @Controller('products')
 export class ProductsController {
@@ -32,17 +33,20 @@ export class ProductsController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', IdValidationPipe) id: string) {
     return this.productsService.findOne(+id)
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+  @Put(':id')
+  update(
+    @Param('id', IdValidationPipe) id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
     return this.productsService.update(+id, updateProductDto)
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', IdValidationPipe) id: string) {
     return this.productsService.remove(+id)
   }
 }
--- b_tutorial/backend/src/products/products.service.ts
+++ b_tutorial/backend/src/products/products.service.ts
@@ -62,15 +62,46 @@ export class ProductsService {
     }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`
+  async findOne(id: number) {
+    const product = await this.productRepository.findOne({
+      where: { id },
+      relations: {
+        category: true,
+      },
+    })
+
+    if (!product) {
+      throw new NotFoundException(`El producto id: ${id} no fue encontrado`)
+    }
+
+    return product
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const product = await this.findOne(id)
+    //! Mutamos lo q retorna findOne para poder guardarlo en la bd
+    Object.assign(product, updateProductDto)
+
+    //! Para actualizar las relaciones, se valida  si existe antes de guardar
+    if (updateProductDto.categoryId) {
+      const category = await this.categoryRepository.findOneBy({
+        id: updateProductDto.categoryId,
+      })
+
+      if (!category) {
+        const errors: string[] = []
+        errors.push('La categoría no existe')
+        throw new NotFoundException(errors)
+      }
+    }
+
+    return await this.productRepository.save(product)
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`
+  async remove(id: number) {
+    const product = await this.findOne(id)
+    await this.productRepository.remove(product)
+
+    return `Producto eliminado`
   }
 }

fde87efb46ab7830e7cf5465d0c6f90d47d9d714
NahumFGz <ngutierrez2507@gmail.com>
2025-04-13 17:52:21 -0500

    feat: :sparkles: Agregando post de datos anidados

--- b_tutorial/backend/src/transactions/transactions.service.ts
+++ b_tutorial/backend/src/transactions/transactions.service.ts
@@ -1,26 +1,64 @@
-import { Injectable } from '@nestjs/common';
-import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { Injectable } from '@nestjs/common'
+import { CreateTransactionDto } from './dto/create-transaction.dto'
+import { UpdateTransactionDto } from './dto/update-transaction.dto'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Transaction, TransactionContents } from './entities/transaction.entity'
+import { Repository } from 'typeorm'
+import { Product } from 'src/products/entities/product.entity'
 
 @Injectable()
 export class TransactionsService {
-  create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
+  constructor(
+    @InjectRepository(Transaction)
+    private readonly transactionRepository: Repository<Transaction>,
+
+    @InjectRepository(TransactionContents)
+    private readonly transactionContentsRepository: Repository<TransactionContents>,
+
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createTransactionDto: CreateTransactionDto) {
+    const transaction = new Transaction()
+    transaction.total = createTransactionDto.total
+    await this.transactionRepository.save(transaction)
+
+    for (const contents of createTransactionDto.contents) {
+      const product = await this.productRepository.findOneBy({
+        id: contents.productId,
+      })
+
+      if (!product) {
+        throw new Error(`Producto con ID ${contents.productId} no encontrado`)
+      }
+      product.inventory -= contents.quantity
+
+      const transactionContent = new TransactionContents()
+      transactionContent.quantity = contents.quantity
+      transactionContent.price = contents.price
+      transactionContent.transaction = transaction
+      transactionContent.product = product
+
+      await this.transactionContentsRepository.save(transactionContent)
+    }
+
+    return 'Venta almacenada correctamente'
   }
 
   findAll() {
-    return `This action returns all transactions`;
+    return `This action returns all transactions`
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} transaction`;
+    return `This action returns a #${id} transaction`
   }
 
   update(id: number, updateTransactionDto: UpdateTransactionDto) {
-    return `This action updates a #${id} transaction`;
+    return `This action updates a #${id} transaction`
   }
 
   remove(id: number) {
-    return `This action removes a #${id} transaction`;
+    return `This action removes a #${id} transaction`
   }
 }

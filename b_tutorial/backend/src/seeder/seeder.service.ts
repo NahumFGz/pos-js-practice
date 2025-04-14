6751bf7907148613ec206f2eec3f33a4a71cde68
NahumFGz <ngutierrez2507@gmail.com>
2025-04-14 03:46:50 -0500

    feat: :sparkles: Agregando el servicio para cargar categorias y producto

--- b_tutorial/backend/src/seeder/seeder.service.ts
+++ b_tutorial/backend/src/seeder/seeder.service.ts
@@ -1,8 +1,46 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Category } from '../categories/entities/category.entity'
+import { Repository, DataSource } from 'typeorm'
+import { Product } from '../products/entities/product.entity'
+import { categories } from './data/categories'
+import { products } from './data/products'
 
 @Injectable()
 export class SeederService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+    private dataSource: DataSource,
+  ) {}
+
+  async onModuleInit() {
+    const connection = this.dataSource
+    await connection.dropDatabase()
+    await connection.synchronize()
+  }
+
   async seed() {
-    console.log('Desde seed en seeder.service.ts')
+    await this.categoryRepository.save(categories)
+    for (const seedProduct of products) {
+      const category = await this.categoryRepository.findOneBy({
+        id: seedProduct.categoryId,
+      })
+      if (!category) {
+        throw new Error(
+          `Categoría con ID ${seedProduct.categoryId} no encontrada`,
+        )
+      }
+
+      const product = new Product()
+      product.name = seedProduct.name
+      product.image = seedProduct.image
+      product.price = seedProduct.price
+      product.inventory = seedProduct.inventory
+      product.category = category
+      await this.productRepository.save(product)
+    }
   }
 }

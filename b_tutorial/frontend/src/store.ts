ec4def973c5ea2261806f1cd010d59914db4a74a
NahumFGz <ngutierrez2507@gmail.com>
2025-04-15 00:53:01 -0500

    fix: :ambulance: Corrección de set, dev

--- b_tutorial/frontend/src/store.ts
+++ b_tutorial/frontend/src/store.ts
@@ -9,11 +9,23 @@ interface Store {
 }
 
 export const useStore = create<Store>()(
-  devtools((set, dev) => ({
+  devtools((set, get) => ({
     total: 0,
     contents: [],
     addToCart: (product) => {
-      console.log('desde addToCart', product)
+      const { id: productId, categoryId: _categoryId, ...data } = product
+      let contents: ShoppingCart = []
+
+      contents = [
+        ...get().contents, //Get se usa para llamar a la variable del store
+        {
+          ...data,
+          quantity: 1,
+          productId,
+        },
+      ]
+
+      set(() => ({ contents })) //Set sirve para asignar el valor en memoria al estado
     },
   }))
 )

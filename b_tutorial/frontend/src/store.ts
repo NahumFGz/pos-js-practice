ab44825c672e619c7475a801dd1a04d5b1f3ce2f
NahumFGz <ngutierrez2507@gmail.com>
2025-04-14 20:26:51 -0500

    feat: :sparkles: Agregando store con zustand

--- b_tutorial/frontend/src/store.ts
+++ b_tutorial/frontend/src/store.ts
@@ -1,9 +1,15 @@
 import { create } from 'zustand'
+import { Product } from './schemas'
 
 interface Store {
   total: number
+
+  addToCart: (product: Product) => void
 }
 
 export const useStore = create<Store>(() => ({
   total: 0,
+  addToCart: (product) => {
+    console.log('desde addToCart', product)
+  },
 }))

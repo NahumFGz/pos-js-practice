04cb7a87b7dcd0865da28d677aa59f3212cff090
NahumFGz <ngutierrez2507@gmail.com>
2025-04-14 19:44:21 -0500

    feat: :sparkles: Agregando los productos

--- b_tutorial/frontend/app/(store)/[categoryId]/page.tsx
+++ b_tutorial/frontend/app/(store)/[categoryId]/page.tsx
@@ -1,12 +1,28 @@
+import { CategoryWithProductsResponseSchema } from '@/app/src/schemas'
+import ProductCard from '@/components/products/ProductCard'
+
 type Params = Promise<{ categoryId: string }>
+
+async function getProducts(categoryId: string) {
+  const url = `${process.env.API_URL}/categories/${categoryId}?products=true`
+  const req = await fetch(url)
+  const json = await req.json()
+
+  const products = CategoryWithProductsResponseSchema.parse(json)
+  return products
+}
 
 export default async function StorePage({ params }: { params: Params }) {
   const { categoryId } = await params
-  console.log(categoryId)
+  const category = await getProducts(categoryId)
 
   return (
     <>
-      <div>StorePage</div>
+      <div className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3'>
+        {category.products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </>
   )
 }
---
+++ b_tutorial/frontend/app/src/schemas.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod'
+
+// !Esquemas de APIs
+export const ProductSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  image: z.string(),
+  price: z.coerce.number(),
+  inventory: z.number(),
+  categoryId: z.number(),
+})
+
+export const CategorySchema = z.object({
+  id: z.number(),
+  name: z.string(),
+})
+
+export const CategoryWithProductsResponseSchema = CategorySchema.extend({
+  products: z.array(ProductSchema),
+})
+
+// !Types
+export type Product = z.infer<typeof ProductSchema>

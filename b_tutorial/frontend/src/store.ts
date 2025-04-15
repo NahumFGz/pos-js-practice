363559bd4cbcb13733b8a0678260192542506a0f
NahumFGz <ngutierrez2507@gmail.com>
2025-04-15 01:02:49 -0500

    feat: :sparkles: Se agrega el store del carrito

--- b_tutorial/frontend/src/store.ts
+++ b_tutorial/frontend/src/store.ts
@@ -16,16 +16,42 @@ export const useStore = create<Store>()(
       const { id: productId, categoryId: _categoryId, ...data } = product
       let contents: ShoppingCart = []
 
-      contents = [
-        ...get().contents, //Get se usa para llamar a la variable del store
-        {
-          ...data,
-          quantity: 1,
-          productId,
-        },
-      ]
+      //* Verificar si el elemento ya se encuentra en el estado
+      const duplicated = get().contents.findIndex(
+        (item) => item.productId === productId
+      )
 
-      set(() => ({ contents })) //Set sirve para asignar el valor en memoria al estado
+      if (duplicated >= 0) {
+        //Verificar la cantidad de inventario para no exceder
+        if (
+          get().contents[duplicated].quantity >=
+          get().contents[duplicated].inventory
+        )
+          return
+
+        // Buscar el producto y agregar
+        contents = get().contents.map((item) =>
+          item.productId === productId
+            ? {
+                ...item,
+                quantity: item.quantity + 1,
+              }
+            : item
+        )
+      } else {
+        contents = [
+          //* Get se usa para llamar a la variable del store
+          ...get().contents,
+          {
+            ...data,
+            quantity: 1,
+            productId,
+          },
+        ]
+      }
+
+      //*Set sirve para asignar el valor en memoria al estado
+      set(() => ({ contents }))
     },
   }))
 )

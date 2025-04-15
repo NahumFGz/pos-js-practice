43443b38937099fee531af29c73c91f77e5e117e
NahumFGz <ngutierrez2507@gmail.com>
2025-04-15 01:20:31 -0500

    feat: :sparkles: Agregando ShoppingCartItem

--- b_tutorial/frontend/src/schemas.ts
+++ b_tutorial/frontend/src/schemas.ts
@@ -36,3 +36,4 @@ export const ShoppingCartSchema = z.array(ShoppingCartContentSchema)
 // !Types
 export type Product = z.infer<typeof ProductSchema>
 export type ShoppingCart = z.infer<typeof ShoppingCartSchema>
+export type CartItem = z.infer<typeof ShoppingCartContentSchema>

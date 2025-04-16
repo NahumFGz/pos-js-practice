3d5a129ac56716424ba8458886c950a5ddd6a7b8
NahumFGz <ngutierrez2507@gmail.com>
2025-04-16 17:36:16 -0500

    feat: :sparkles: Agregando dynamic

--- b_tutorial/frontend/components/transactions/TransactionFilter.tsx
+++ b_tutorial/frontend/components/transactions/TransactionFilter.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css'
 import { useState } from 'react'
 import { format } from 'date-fns'
@@ -8,6 +7,12 @@ import { useQuery } from '@tanstack/react-query'
 import { getSalesByDate } from '@/src/api'
 import TransactionSummary from './TransactionSumary'
 import { formatCurrency } from '@/src/utils'
+import dynamic from 'next/dynamic'
+
+//! Este componente cargará 100% en el cliente, en caso no se pueda personalizar con locale
+const Calendar = dynamic(() => import('react-calendar'), {
+  ssr: false,
+})
 
 type ValuePiece = Date | null
 type Value = ValuePiece | [ValuePiece, ValuePiece]
@@ -25,12 +30,12 @@ export default function TransactionFilter() {
     data?.reduce((total, transaction) => total + +transaction.total, 0) ?? 0
 
   return (
-    <div className='grid grid-cols-1 lg:grid-cols-2 gap-5 mt-10'>
-      <div>
+    <div className='grid grid-cols-1 lg:grid-cols-2 gap-5 mt-10 relative items-start'>
+      <div className='lg:sticky lg:top-10'>
         <Calendar
           value={date}
           onChange={setDate}
-          locale='es-ES' // <-- Forzar el idioma desde el servidor
+          //locale='es-ES' // <-- Forzar el idioma desde el servidor
         />
       </div>
 

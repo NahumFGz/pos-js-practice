9b4248332e7c8a73115ca1b55da8e493b548fd33
NahumFGz <ngutierrez2507@gmail.com>
2025-04-16 17:06:49 -0500

    feat: :sparkles: Se agraga el prefetchqueryy usequery

--- b_tutorial/frontend/components/transactions/TransactionFilter.tsx
+++ b_tutorial/frontend/components/transactions/TransactionFilter.tsx
@@ -4,6 +4,8 @@ import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css'
 import { useState } from 'react'
 import { format } from 'date-fns'
+import { useQuery } from '@tanstack/react-query'
+import { getSalesByDate } from '@/src/api'
 
 type ValuePiece = Date | null
 type Value = ValuePiece | [ValuePiece, ValuePiece]
@@ -12,7 +14,13 @@ export default function TransactionFilter() {
   const [date, setDate] = useState<Value>(new Date())
 
   const formattedDate = format(date?.toString() || new Date(), 'yyyy-MM-dd')
-  console.log(formattedDate)
+  const { data, isLoading } = useQuery({
+    queryKey: ['sales', formattedDate],
+    queryFn: () => getSalesByDate(formattedDate),
+  })
+
+  console.log('data', data)
+  console.log(isLoading)
 
   return (
     <div className='grid grid-cols-1 lg:grid-cols-2 gap-5 mt-10'>

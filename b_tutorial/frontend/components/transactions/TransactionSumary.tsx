ef2ec36eeb980318fa71c399d7362b194f2451fc
NahumFGz <ngutierrez2507@gmail.com>
2025-04-16 17:26:01 -0500

    feat: :sparkles: Agregando los campos de los datos de la API

--- b_tutorial/frontend/components/transactions/TransactionSumary.tsx
+++ b_tutorial/frontend/components/transactions/TransactionSumary.tsx
@@ -1,4 +1,6 @@
 import { Transaction } from '@/src/schemas'
+import { formatCurrency } from '@/src/utils'
+import Image from 'next/image'
 
 export default function TransactionSummary({
   transaction,
@@ -7,7 +9,7 @@ export default function TransactionSummary({
 }) {
   return (
     <>
-      <div className='mt-6  text-sm font-medium text-gray-500 border border-gray-200'>
+      <div className='mb-6  text-sm font-medium text-gray-500 border border-gray-200'>
         <p className='text-sm font-black text-gray-900 p-2 bg-gray-200 '>
           ID: {transaction.id}
         </p>
@@ -18,11 +20,22 @@ export default function TransactionSummary({
           {transaction.contents.map((item) => (
             <li key={item.id} className='p-5 '>
               <div className='flex items-center space-x-6 '>
-                <div className='relative w-32 h-32'></div>
+                <div className='relative w-32 h-32'>
+                  <Image
+                    src={`${process.env.NEXT_PUBLIC_API_URL}/img/${item.product.image}`}
+                    alt={`Imagen de producto ${item.product.name}`}
+                    className='absolute'
+                    fill
+                  />
+                </div>
                 <div className='flex-auto space-y-1 '>
-                  <h3 className='text-gray-900'></h3>
-                  <p className='text-lg font-extrabold  text-gray-900'></p>
-                  <p className='text-lg  text-gray-900'>Cantidad: </p>
+                  <h3 className='text-gray-900'>{item.product.name}</h3>
+                  <p className='text-lg font-extrabold  text-gray-900'>
+                    {formatCurrency(+item.price)}
+                  </p>
+                  <p className='text-lg  text-gray-900'>
+                    Cantidad: {item.quantity}
+                  </p>
                 </div>
               </div>
             </li>
@@ -30,19 +43,28 @@ export default function TransactionSummary({
         </ul>
 
         <dl className='space-y-6  text-sm font-medium text-gray-500 p-5'>
-          <div className='flex justify-between'>
-            <dt>Cupón Utilizado</dt>
-            <dd className='text-gray-900'></dd>
-          </div>
+          {transaction.coupon && (
+            <>
+              <div className='flex justify-between'>
+                <dt>Cupón Utilizado</dt>
+                <dd className='text-gray-900'>{transaction.coupon}</dd>
+              </div>
 
-          <div className='flex justify-between'>
-            <dt>Descuento</dt>
-            <dd className='text-gray-900'>-</dd>
-          </div>
+              <div className='flex justify-between'>
+                <dt>Descuento</dt>
+                <dd className='text-gray-900'>
+                  {transaction.discount &&
+                    formatCurrency(+transaction.discount)}
+                </dd>
+              </div>
+            </>
+          )}
 
           <div className='flex justify-between'>
             <dt className='text-lg text-black font-black'>Total</dt>
-            <dd className='text-lg text-black font-black'></dd>
+            <dd className='text-lg text-black font-black'>
+              {formatCurrency(+transaction.total)}
+            </dd>
           </div>
         </dl>
       </div>

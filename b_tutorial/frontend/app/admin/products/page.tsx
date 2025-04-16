e61888b3cafb824be52d159e7da6a80dfdedb5fc
NahumFGz <ngutierrez2507@gmail.com>
2025-04-16 17:59:33 -0500

    feat: :sparkles: Agregando tabla de productos

--- b_tutorial/frontend/app/admin/products/page.tsx
+++ b_tutorial/frontend/app/admin/products/page.tsx
@@ -1,7 +1,27 @@
-export default function ProductsPage() {
+import ProductsTable from '@/components/products/ProductsTable'
+import Heading from '@/components/ui/Heading'
+import { ProductsResponseSchema } from '@/src/schemas'
+
+async function getProducts() {
+  const url = `${process.env.API_URL}/products`
+  const req = await fetch(url)
+  const json = await req.json()
+  const data = ProductsResponseSchema.parse(json)
+
+  return {
+    products: data.products,
+    total: data.total,
+  }
+}
+
+export default async function ProductsPage() {
+  const { products, total } = await getProducts()
+
   return (
     <>
-      <p>ProductsPage</p>
+      <Heading>Administar Productos</Heading>
+
+      <ProductsTable products={products} />
     </>
   )
 }

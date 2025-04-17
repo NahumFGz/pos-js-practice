efe2d6bfb454487721a66e792fbb51db7e7f7496
NahumFGz <ngutierrez2507@gmail.com>
2025-04-16 19:29:14 -0500

    feat: :zap: Iniciando edit products

---
+++ b_tutorial/frontend/app/admin/products/[id]/not-found.tsx
@@ -0,0 +1,17 @@
+import Heading from '@/components/ui/Heading'
+import Link from 'next/link'
+
+export default function NotFound() {
+  return (
+    <div className='text-center'>
+      <Heading>Producto No encontrado</Heading>
+
+      <p>
+        Tal vez quieras volver a{' '}
+        <Link className='text-green-400' href={'/admin/products?page=1'}>
+          Products
+        </Link>
+      </p>
+    </div>
+  )
+}
---
+++ b_tutorial/frontend/components/products/EditProductForm.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useActionState, useEffect } from 'react'
+import { toast } from 'react-toastify'
+
+export default function EditProductForm({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const router = useRouter()
+
+  return (
+    <form className='space-y-5'>
+      {children}
+      <input
+        type='submit'
+        className='rounded bg-green-400 font-bold py-2 w-full cursor-pointer'
+        value='Guardar Cambios'
+      />
+    </form>
+  )
+}
